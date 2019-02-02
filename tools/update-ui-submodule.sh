5ac34724937db1aa6e1fa6e3afe123898d214130
Mario Gómez <mxgxw.alpha@gmail.com>
2019-02-02 17:31:16 -0600

    Script de update de ui actualizado a ruta relativa

--- tools/update-ui-submodule.sh
+++ tools/update-ui-submodule.sh
@@ -2,11 +2,12 @@
 echo Pulling last updates from FrontEnd Repo...
 cd ..
 cd contemosnosotros-ui
+git checkout master
 git pull
 echo Installing Dependencies
 npm i
 echo Creating build
 npm run build
-echo Syncing Deploying files to /var/html
-rsync -avr --omit-dir-times --no-perms --ignore-times build/ /var/www/html
+echo Syncing Deploying files to Cake Home
+rsync -avr --omit-dir-times --no-perms --ignore-times build/ ../contemosnosotros/webroot
 
